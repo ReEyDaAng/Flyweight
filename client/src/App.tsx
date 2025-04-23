d7d1a845ccb3f07fffcc3639294d5df29ff517ec
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-04-23 22:37:15 +0300

    Initialize ShadCN

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "@/components/ui/Button";
 
 function App() {
-  return <>Hello, World!</>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
